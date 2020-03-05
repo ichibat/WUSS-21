feaf537377c0b3f55637ae398b8a405d6246522e
tamotsu ichiba <ichibat@gmail.com>
2020-03-05 09:15:46 +0900

    first commit

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const path = require('path');
 const cp = require('child_process');
 const ngrok = require('ngrok');
 
+
 // create LINE SDK config from env variables
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
